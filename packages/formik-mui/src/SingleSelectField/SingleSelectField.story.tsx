33289f6499531ea4e16af9e37eae4cd9b9a9d731
Naresh Bhatia <naresh.archfirst@gmail.com>
2020-05-24 22:02:36 -0400

    fix(date-utils): export TzUtils

--- packages/formik-mui/src/SingleSelectField/SingleSelectField.story.tsx
+++ packages/formik-mui/src/SingleSelectField/SingleSelectField.story.tsx
@@ -2,79 +2,17 @@ import React, { Fragment, useState } from 'react';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import { Timezone, TzUtils } from '@react-force/date-utils';
 import { storiesOf } from '@storybook/react';
 import { Form, Formik } from 'formik';
 import * as yup from 'yup';
 import { StoryDecorator } from '../stories';
 import { SingleSelectField } from './SingleSelectField';
 
-import { DateUtils } from '@react-force/date-utils';
-import moment from 'moment';
-import 'moment-timezone';
-
-const { gmtOffset, tzAbbr } = DateUtils;
+const { findTimezone, getTimezones } = TzUtils;
 
 // Default timezone
 const DefaultTz = 'America/New_York';
-
-/**
- * Timezone
- *
- * Example:
- *   {
- *     name: 'America/New_York',
- *     label: '(GMT-04:00) America/New_York - EDT'
- *   }
- */
-interface Timezone {
-    name: string;
-    label: string;
-}
-
-/**
- * Returns a descriptive label for the specified timezone name
- * @param tzName
- */
-function getTimezoneLabel(tzName: string) {
-    return `(${gmtOffset(tzName)}) ${tzName} - ${tzAbbr(tzName)}`;
-}
-
-/**
- * Returns an array of Timezones
- */
-function getTimezones(): Array<Timezone> {
-    return moment.tz
-        .names()
-        .map((name) => {
-            const now = Date.now();
-            const zone = moment.tz.zone(name);
-            return { name, offset: zone !== null ? zone.utcOffset(now) : 0 };
-        })
-        .sort((a, b) =>
-            a.offset === b.offset
-                ? a.name.localeCompare(b.name)
-                : b.offset - a.offset
-        )
-        .map((zone) => {
-            const name = zone.name;
-            return {
-                name,
-                label: getTimezoneLabel(name),
-            };
-        });
-}
-
-/**
- * Finds a timezone by name
- * @param timezones
- * @param tzName
- */
-function findTimezone(
-    timezones: Array<Timezone>,
-    tzName: string
-): Timezone | undefined {
-    return timezones.find((timezone) => timezone.name === tzName);
-}
 
 const timezones = getTimezones();
 
