ec99499dc4ea1b84feca57751418dbfb44052ca8
Naresh Bhatia <naresh.archfirst@gmail.com>
2020-06-22 11:10:41 -0400

    refactor(models): Removed unused type EntityFactory

--- packages/models/src/Entity.ts
+++ packages/models/src/Entity.ts
@@ -8,12 +8,6 @@ export interface Entity {
 }
 
 /**
- * A function that returns a new entity of type T. The optional parameter type
- * can be used to control the exact type of entity that is created.
- */
-export type EntityFactory<T extends Entity> = (type?: string) => T;
-
-/**
  * A function that takes an Entity and returns a jsValue
  */
 export type Serializer<T extends Entity> = (entity: T) => any;
