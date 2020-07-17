1e5395d9224dc662c029e9205fbce94cfbc06a7d
Naresh Bhatia <naresh.archfirst@gmail.com>
2020-07-16 21:27:03 -0400

    feat(mock-data): Added product helpers

--- packages/mock-data/src/Product.ts
+++ packages/mock-data/src/Product.ts
@@ -17,3 +17,47 @@ export const newProduct = (): Product => ({
     price: 0,
     isFeatured: false,
 });
+
+/**
+ * Finds a product in an array
+ */
+export const findProduct = (products: Array<Product>, id: string) =>
+    products.find((product) => product.id === id);
+
+/**
+ * Adds a new product to an array of products and return a new array
+ * (immutable update)
+ */
+export const addProduct = (products: Array<Product>, productToAdd: Product) =>
+    products.slice().push(productToAdd);
+
+/**
+ * Updates a product in an array of products and return a new array
+ * (immutable update)
+ */
+export const updateProduct = (
+    products: Array<Product>,
+    productToUpdate: Product
+) =>
+    products.map((product) =>
+        product.id === productToUpdate.id ? productToUpdate : product
+    );
+
+/**
+ * Removes a product from an array of products and return a new array
+ * (immutable update)
+ */
+export const removeProduct = (
+    products: Array<Product>,
+    productToRemove: Product
+) => products.filter((product) => product.id !== productToRemove.id);
+
+/**
+ * Sorts products by (manufacturer, name) and returns a new array
+ * (immutable update)
+ */
+export const sortProducts = (products: Array<Product>) =>
+    products.slice().sort((a: Product, b: Product) => {
+        const result = a.manufacturer.localeCompare(b.manufacturer);
+        return result !== 0 ? result : a.name.localeCompare(b.name);
+    });
